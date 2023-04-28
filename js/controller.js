bd735da3025c2f6de0e0f23f20cc5e20d01fa304
ogasho25 <116321269+ogasho25@users.noreply.github.com>
2023-04-28 15:43:11 +0900

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -3,8 +3,8 @@ document.body.onkeydown = function( e ) {
         37: 'left',
         39: 'right',
         40: 'down',
-        38: 'rotate',
-        32: 'drop'
+        32: 'rotate',
+        38: 'drop'
     };
     if ( typeof keys[ e.keyCode ] != 'undefined' ) {
         keyPress( keys[ e.keyCode ] );
